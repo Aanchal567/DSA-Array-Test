317e4888fd4916a264dbfbaf5a5f3e53a471aec9
Aanchal567 <119961936+Aanchal567@users.noreply.github.com>
2025-12-18 13:52:12 +0530

    Implement array input and max/min calculation
    Added functionality to input an array and find its max and min values.

--- 1ques.py
+++ 1ques.py
@@ -1,3 +1,13 @@
-a=4
-b=5
-print(a+b) 
+n=int(input("Enter the input of the array: "))
+arr = []
+
+for i in range(n):
+    arr.append(int(input(f"Enter element {i+1}: ")))
+print("Your array:", arr)
+max=float("-inf")
+min=float("inf")
+for i in range(0,n):
+    if arr[i]>max:
+        min=max
+        max=arr[i]
+print(max,min)
